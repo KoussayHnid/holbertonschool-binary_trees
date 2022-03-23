4852137b35d070572d759b0c2df056a272fff661
Mohamed <91054983+MOUHAMEDelHakim@users.noreply.github.com>
2022-03-24 00:29:41 +0100

    Tree leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,12 +6,12 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t k = 0;
+    size_t s = 0;
 
     if (tree = NULL)
         return (0);
     if (tree->left == NULL && tree-> right == NULL)
         return(1);
-    k = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
-    return(k);
+    s = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+    return(s);
 }
